4cd75e9d835b5345260879b835697b09dbb5b7f8
JunHyung1206 <pjh120610@gamil.com>
2022-10-27 02:07:43 +0000

    RobertModel add

--- model/model.py
+++ model/model.py
@@ -2,6 +2,8 @@ import transformers
 import torch
 import torchmetrics
 import pytorch_lightning as pl
+import torch.nn as nn
+
 
 from . import loss as loss_module
 
@@ -23,6 +25,81 @@ class Model(pl.LightningModule):
     def forward(self, x):
         x = self.plm(x)['logits']
 
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = self.loss_func(logits, y.float())
+        self.log("train_loss", loss)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = self.loss_func(logits, y.float())
+        self.log("val_loss", loss)
+
+        self.log("val_pearson", torchmetrics.functional.pearson_corrcoef(
+            logits.squeeze(), y.squeeze()))
+
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+
+        self.log("test_pearson", torchmetrics.functional.pearson_corrcoef(
+            logits.squeeze(), y.squeeze()))
+
+    def predict_step(self, batch, batch_idx):
+        x = batch
+        logits = self(x)
+
+        return logits.squeeze()
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr)
+        return optimizer
+
+
+class HeadClassifier(nn.Module):
+
+    def __init__(self, hidden_dim, drop_out_rate):
+        super().__init__()
+        self.dense = nn.Linear(768, hidden_dim)
+        self.dropout = nn.Dropout(drop_out_rate)
+        self.output = nn.Linear(hidden_dim, 1)
+
+    def forward(self, features):
+        x = features[:, 0, :]
+        x = self.dropout(x)
+        x = self.dense(x)
+        x = torch.tanh(x)
+        x = self.dropout(x)
+        x = self.output(x)
+        return x
+
+
+class RoBERTa_Base_Model(pl.LightningModule):
+    def __init__(self, model_name, lr):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.model_name = model_name
+        self.lr = lr
+
+        # 사용할 모델을 호출합니다.
+        self.plm = transformers.AutoModel.from_pretrained(
+            pretrained_model_name_or_path=model_name)
+        self.classifier = HeadClassifier(1024, 0.2)
+        # Loss 계산을 위해 사용될 L1Loss를 호출합니다.
+        self.loss_func = loss_module.L1_loss
+
+    def forward(self, x):
+        x = self.plm(x)[0]
+        x = self.classifier(x)
         return x
 
     def training_step(self, batch, batch_idx):
