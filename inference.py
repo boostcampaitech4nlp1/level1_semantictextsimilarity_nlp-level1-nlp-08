29b00bfdab175c07e6d8096c6627abeab2ca462f
jihoon <jh921204@naver.com>
2022-10-27 07:22:29 +0000

    model.pt, output.csv, submit.csv add in gitignore

--- inference.py
+++ inference.py
@@ -7,7 +7,6 @@ import torch
 import pytorch_lightning as pl
 from data_loader.data_loaders import Dataloader
 import model.model as module_arch
-from utils import utils
 
 
 # fix random seeds for reproducibility
@@ -46,8 +45,10 @@ if __name__ == '__main__':
     
     # Inference part
     # 저장된 모델로 예측을 진행합니다.
-    model = module_arch.Model(args.model_name, args.learning_rate, args.bce)
-    model.load_from_checkpoint(args.saved_model)
+    # model = module_arch.Model(args.model_name, args.learning_rate, args.bce)
+    # model.load_from_checkpoint(args.saved_model)
+    model = torch.load(args.saved_model)
+    model.eval()
     print("###### 모델 불러오기 완료 ######")
     predictions = trainer.predict(model=model, datamodule=dataloader)
 
@@ -56,4 +57,7 @@ if __name__ == '__main__':
     # output 형식을 불러와서 예측된 결과로 바꿔주고, output.csv로 출력합니다.
     output = pd.read_csv('../data/test.csv')
     output['target'] = predictions
-    output.to_csv('output_{args.model_name}.csv', index=False)
+    output.to_csv('output.csv', index=False)
+    
+    submit = output[['id', 'target']]
+    submit.to_csv('submit.csv', index=False)
