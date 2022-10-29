9385b8828a9f41c4d7794e76ad0a438a53c7332a
JunHyung1206 <pjh120610@naver.com>
2022-10-29 10:40:56 +0000

    인자 변경 반영하여 수정, swap 부분 추가 구현 반영

--- inference.py
+++ inference.py
@@ -5,17 +5,18 @@ from data_loader.data_loaders import Dataloader
 import model.model as module_arch
 
 
-def inference(args):
+def inference(args, cfg):
     dataloader = Dataloader(
-        args.model_name,
-        args.batch_size,
-        args.train_ratio,
-        args.shuffle,
-        args.train_path,
-        args.test_path,
-        args.predict_path,
+        cfg.model.model_name,
+        cfg.train.batch_size,
+        cfg.data.train_ratio,
+        cfg.data.shuffle,
+        cfg.path.train_path,
+        cfg.path.test_path,
+        cfg.path.predict_path,
+        cfg.data.swap,
     )
-    trainer = pl.Trainer(gpus=1, max_epochs=args.max_epoch, log_every_n_steps=1)
+    trainer = pl.Trainer(gpus=1, max_epochs=cfg.train.max_epoch, log_every_n_steps=1)
 
     if args.saved_model.split(".")[-1] == "ckpt":
         model_name = "/".join(args.saved_model.split("/")[1:3]).split("_")[
@@ -23,10 +24,10 @@ def inference(args):
         ]  # huggingface에 저장된 모델명을 parsing함
         model = module_arch.Model(
             model_name,
-            args.learning_rate,
-            args.loss,
+            cfg.train.learning_rate,
+            cfg.train.loss,
             dataloader.new_vocab_size(),
-            args.frozen,
+            cfg.train.use_frozen,
         )  # 새롭게 추가한 토큰 사이즈 반영
 
         model = model.load_from_checkpoint(args.saved_model)
