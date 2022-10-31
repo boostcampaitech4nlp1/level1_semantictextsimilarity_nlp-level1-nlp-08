b967ad4f5c2e74c86e97c97dc00e0d0bfb175785
root <jjn00712@gmail.com>
2022-10-31 01:45:57 +0000

    feat: trainer.test( ) 추가 및 후처리 정리
    - trainer.test 추가하여 test_pearson 점수 출력되도록 수정
    - predict 결과 후처리 수정
      - 정규화할 때 predict 결과 그대로 활용

--- inference.py
+++ inference.py
@@ -19,31 +19,25 @@ def inference(args, conf):
     )
     trainer = pl.Trainer(gpus=1, max_epochs=conf.train.max_epoch, log_every_n_steps=1)
 
-    model, _, __ = train.load_model(
-        args, conf, dataloader
-    )  # train.py에 저장된 모델을 불러오는 메서드 따로 작성함
+    model, _, __ = train.load_model(args, conf, dataloader)  # train.py에 저장된 모델을 불러오는 메서드 따로 작성함
 
     model.eval()
 
-    predictions = trainer.predict(model=model, datamodule=dataloader)
+    predictions = trainer.predict(
+        model=model,
+        datamodule=dataloader,
+    )
+    trainer.test(model=model, datamodule=dataloader)
 
-    predictions = list(round(float(i), 1) for i in torch.cat(predictions))
-    predictions_n = [
-        round(5 * x / (max(predictions) - min(predictions) + 1e-8), 1)
-        for x in predictions
-    ]  # Normalize
-    predictions_e = [
-        round((x[0] + x[1]) / 2, 1) for x in zip(predictions, predictions_n)
-    ]  # Mean
+    predictions = list(float(i) for i in torch.cat(predictions))  # 리스트화
+    predictions_b = list(round(float(i), 1) for i in predictions)  # base postprocess
+    predictions_n = [round(5 * x / (max(predictions) - min(predictions) + 1e-8), 1) for x in predictions]  # Normalize
 
-    output = pd.read_csv("../data/sample_submission.csv")
+    output_b = pd.read_csv("../data/sample_submission.csv")
     output_n = pd.read_csv("../data/sample_submission.csv")
-    output_e = pd.read_csv("../data/sample_submission.csv")
 
-    output["target"] = predictions
+    output_b["target"] = predictions_b
     output_n["target"] = predictions_n
-    output_e["target"] = predictions_e
 
-    output.to_csv("output.csv", index=False)
+    output_b.to_csv("output_b.csv", index=False)
     output_n.to_csv("output_n.csv", index=False)
-    output_e.to_csv("output_e.csv", index=False)
